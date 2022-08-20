c9483998d19a88a20b4324eb1e6a8801fe748f8e
Rakibul Islam <rakibul241221@gmail.com>
2022-08-21 01:47:27 +0600

    complete project added

--- js/shopping.js
+++ js/shopping.js
@@ -4,15 +4,35 @@ function displayCart(carrtProduct) {
     console.log(carrtProduct)
 
     const tableBody = document.getElementById('products-cart');
-
+    tableBody.innerHTML = '';
+    let totalPrice = 0;
     for (let i = 0; i < carrtProduct.length; i++) {
         console.log(cartArray[i].productName, cartArray[i].productPrice)
 
         const productName = cartArray[i].productName;
         const productPrice = cartArray[i].productPrice;
+        totalPrice = totalPrice + productPrice;
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        
+        <tr>
+        <th>${i + 1}</th>
+        <th>${productName}</th>
+        <th>${productPrice}</th>
+      </tr>`
 
-
+        tableBody.appendChild(tr)
     }
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <tr>
+        <th>Your</th>
+        <th>Total Price</th>
+        <th>${totalPrice}</th>
+      </tr>
+    `
+    tableBody.appendChild(tr)
 }
 
 function addToCart(element) {
